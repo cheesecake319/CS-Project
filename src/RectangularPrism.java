e7ee79bfa1f35d0f9f5035e311e32cfe20fd23c9
student <student@10.11.112.190>
2018-03-15 10:03:52 -0400

    aight

---
+++ src/RectangularPrism.java
@@ -0,0 +1,20 @@
+
+public class RectangularPrism extends Shape{
+	private double width, length, height; 
+	@Override
+	public double getVolume() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	 public RectangularPrism(double BaseWidth, double BaseLength, double height) {
+	        this.width = width;
+	        this.length = length;
+	        this.height = height;
+	 }
+	@Override
+	public double getSurfaceArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
