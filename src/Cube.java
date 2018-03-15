bb77314e4d11b1bd5aa02d5c22638fa82c9bb86f
student <student@10.11.112.190>
2018-03-15 12:07:54 -0400

    boing

---
+++ src/Cube.java
@@ -0,0 +1,8 @@
+
+public class Cube extends RectangularPrism {
+	public Cube(double width, double length, double height) {
+		super(width, length, height);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/Sphere.java
@@ -0,0 +1,16 @@
+
+public class Sphere extends Shape{
+	private double radius, circumference; 
+	@Override
+	public double getVolume() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getSurfaceArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
